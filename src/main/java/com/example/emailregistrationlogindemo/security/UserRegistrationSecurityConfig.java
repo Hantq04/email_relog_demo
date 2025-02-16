458b3a137fe2ac1438285fa5315eac8fd3039193
Hantq04 <hantq04@gmail.com>
2025-02-16 16:40:48 +0700

    relog commit

--- src/main/java/com/example/emailregistrationlogindemo/security/UserRegistrationSecurityConfig.java
+++ src/main/java/com/example/emailregistrationlogindemo/security/UserRegistrationSecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.emailregistrationlogindemo.security;
 import com.example.emailregistrationlogindemo.exception.CustomAuthenticationFailureHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
